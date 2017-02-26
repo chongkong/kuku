bb7652eda6d4dd307de989533e9438447767e0ed
chongkong <chongkong94@gmail.com>
2017-02-26 22:58:16 +0900

    Commit missing file

--- kuku/util.py
+++ kuku/util.py
@@ -10,5 +10,15 @@ def random_alphanumeric(length):
     return ''.join(random.choice(alphanumeric) for _ in range(length))
 
 
+class NamedObject(object):
+    def __repr__(self):
+        return type(self).__name__
+
+
+def create_named_singleton(name):
+    singleton_cls = type(name, (NamedObject,), {})
+    return singleton_cls()
+
+
 if __name__ == '__main__':
     print(random_alphanumeric(10))
